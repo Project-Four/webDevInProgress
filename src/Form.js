666610ddee0685668768396cb4393bccc0ddfd3c
MandeepMann <mandeep_mann30@hotmail.com>
2022-12-10 16:24:54 -0500

    installed axios and added API call in Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,24 @@
 const Form = () => {
 
+    // API call
+    axios({
+    url: 'https://api.unsplash.com/search/photos',
+    params: {
+      method: 'GET',
+      client_id: 'hBNNU3fausksBX8Iir21vcSOZhnQmtoEut-59TPJj7Q',
+      query: 'background',
+      per_page: 6,
+      content_filter: 'high',
+      orientation: 'landscape'
+    }
+  })
+    .then((res) => {
+      console.log(res)
+    })
+
+
+    
+
 }
 
 export default Form
