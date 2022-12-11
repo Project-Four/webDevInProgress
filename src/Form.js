4d95cd321653cac90c845740a6af1298414ca0d8
ThomasHuynh <Thomas_TTH@live.com>
2022-12-11 15:52:14 -0500

    added API call to Form component

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,35 @@
-const Form = () => {
+import axios from 'axios';
+import { useEffect } from 'react';
 
+const Form = () => {
+    
+  useEffect(() => {
+    const apiKey = 'd_goQdbVz1S8P2CqrowdnFHXq8pF0xt7O6Py6INsoAs'
+    
+    axios({
+        url: 'https://api.unsplash.com/search/photos',
+        method: 'GET',
+        dataResponse: 'json',
+        params: {
+            client_id: apiKey,
+            query: 'background',
+            per_page: 6,
+            content_filter: 'high',
+            orientation: 'landscape',
+            alt_description: true
+        }
+        
+    })
+    .then((res) => {
+        setData(res.data.results)
+    })
+}, [])
+    
+      return (
+        <div className="App">
+    
+        </div>
+      );    
 }
 
 export default Form
