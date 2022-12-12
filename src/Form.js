a48c301ddcf1ff14cad4dbad929eae906e01d54a
justin <justin.ng26@hotmail.com>
2022-12-12 13:08:45 +0000

    initial commit

--- src/Form.js
+++ src/Form.js
@@ -1,23 +1,83 @@
+import axios from 'axios';
+import { useEffect, useState } from "react";
+
+
+
 const Form = () => {
 
-    // API call
-    axios({
-    url: 'https://api.unsplash.com/search/photos',
-    params: {
-      method: 'GET',
-      client_id: 'hBNNU3fausksBX8Iir21vcSOZhnQmtoEut-59TPJj7Q',
-      query: 'background',
-      per_page: 6,
-      content_filter: 'high',
-      orientation: 'landscape'
-    }
-  })
-    .then((res) => {
-      console.log(res)
-    })
-
-
+  useEffect(() => {
+    const apiKey = 'umteXz05fepLWaDMu_Yu1DojetM-vqfROB2kZ2JmJq4 '
     
+    axios({
+        url: 'https://api.unsplash.com/search/photos',
+        method: 'GET',
+        dataResponse: 'json',
+        params: {
+            client_id: apiKey,
+            query: 'background',
+            per_page: 6,
+            content_filter: 'high',
+            orientation: 'landscape',
+            alt_description: true
+        }
+        
+    })
+    .then((res) => {
+        setData(res.data.results)
+    })
+}, [])
+
+
+// dynamic form section
+
+// create a piece of state to hold the user input details. 
+const [inputFields, setInputFields] = useState([ {name: '', age:''} ])
+  
+// pass the values of index and event from the onChange function.
+const handleFormChange = (index , event) => {
+
+    // store inoutFields into userData variable.
+    let userData = [...inputFields];
+
+    // target the userData index and userData name of the property.
+    userData[index][event.target.name] = event.target.value
+
+    // store this new userData back into the inputFields array with state.
+    setInputFields(userData);
+}
+
+// allow the user to add more fields.
+const addFields = () => {
+
+}
+
+return(
+    <div>
+      <form>
+        {inputFields.map((input, index) => {
+          return (
+             <div key = {index}>
+                <input 
+                  name ='name'
+                  placeholder='Name'
+                  value = {input.name}
+                  onChange = {event => handleFormChange=(index, event)}
+                />
+                <input
+                  name = 'age'
+                  placeholder='Age'
+                  value = {input.age}
+                  onChange = {event => handleFormChange (index, event)}
+                />
+             </div>
+          )
+       } )}
+      </form>
+
+      <button onClick = {addFields}>Add</button>
+    </div>
+);
+  
 
 }
 
