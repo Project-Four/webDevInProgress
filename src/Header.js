3ddd5d3c9684ab66f2709d67028866750375fc39
esteraolszewski <estera.o@live.ca>
2022-12-13 10:56:24 -0500

    updated Header

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,6 @@
-import { useRef } from "react";
-
 
 const Header = () => {
   
-    const main = useRef(null);
-
-    const scroll = (elementRef) => {
-        window.scrollTo({
-          top: elementRef.current.offsetTop,
-          behavior: 'smooth'
-        })
-      }
 
     return(
         <div className="header">
@@ -19,7 +9,7 @@ const Header = () => {
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt voluptatibus enim similique dolore laborum nisi odit temporibus consequuntur amet. Perspiciatis nemo tempora fugit, facere, eius sapiente, ut inventore ullam corporis consequuntur alias debitis. Odio, assumenda optio. Molestiae esse rerum earum autem eveniet quisquam excepturi voluptate quos error? Cum, ipsum atque?</p>
 
-                <button onClick={() => scroll(main)}>Get Started</button>
+                <button>Get Started</button>
 
             </div>
             
