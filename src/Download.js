6108f97493e85591f33e6396896ab2fd28213548
MandeepMann <mandeep_mann30@hotmail.com>
2022-12-18 18:45:12 -0500

    wrapped entire copy block in one pre tag

--- src/Download.js
+++ src/Download.js
@@ -9,37 +9,39 @@ const Download = ({userBackground, userText, inputFields}) => {
                 <div className="userCopy">
                     <div className="copyHtml">
                         {/* HEAD */}
-                        <p>&lt;!DOCTYPE html&gt;</p>
-                        <p>&lt;html lang="en"&gt;</p>
-                        <p>&lt;head&gt;</p>
-                        <pre><p>    &lt;meta charset="UTF-8" /&gt;</p></pre>
-                        <pre><p>    &lt;meta http-equiv="X-UA-Compatible" content="IE=edge" /&gt;</p></pre>
-                        <pre><p>    &lt;meta name="viewport" content="width=device-width, initial-scale=1.0" /&gt;</p></pre>
-                        <pre><p>    &lt;title&gt;Document&lt;/title&gt;</p></pre>
-                        <p>&lt;/head&gt;</p>
+                        <pre>
+                            <p>&lt;!DOCTYPE html&gt;</p>
+                            <p>&lt;html lang="en"&gt;</p>
+                            <p>&lt;head&gt;</p>
+                            <p>    &lt;meta charset="UTF-8" /&gt;</p>
+                            <p>    &lt;meta http-equiv="X-UA-Compatible" content="IE=edge" /&gt;</p>
+                            <p>    &lt;meta name="viewport" content="width=device-width, initial-scale=1.0" /&gt;</p>
+                            <p>    &lt;title&gt;Document&lt;/title&gt;</p>
+                            <p>&lt;/head&gt;</p>
 
-                        {/* BODY */}
-                        <p>&lt;body&gt;</p>
-                        {/* BACKGROUND CONTAINER */}
-                        <pre><p>    &lt;div class="backgroundContainer"&gt;</p></pre>
-                        {/* WRAPPER */}
-                        <pre><p>        &lt;div class="wrapper"&gt;</p></pre>
-                        {/* TEXT CONTAINER */}
-                        <pre><p>            &lt;div class="textContainer"&gt;</p></pre>
-                        <pre><p>                &lt;h1&gt;{userText.name}&lt;/h1&gt;</p></pre>
-                        <pre><p>                &lt;h2&gt;{userText.subtitle}&lt;/h2&gt;</p></pre>
-                        {/* Map through links */}
-                        {inputFields.map((field, index) => {
-                            return (
-                                <div key={index}>
-                                <pre><p>                &lt;a href="{field.link}"&gt;{field.websiteName}&lt;/a&gt;</p></pre>
-                                </div>
-                            )
-                        })}              
-                        <pre><p>            &lt;/div&gt;</p></pre>
-                        <pre><p>    &lt;/div&gt;</p></pre>          
-                        <p>&lt;/body&gt;</p>
-                        <p>&lt;/html&gt;</p>
+                            {/* BODY */}
+                            <p>&lt;body&gt;</p>
+                            {/* BACKGROUND CONTAINER */}
+                            <p>    &lt;div class="backgroundContainer"&gt;</p>
+                            {/* WRAPPER */}
+                            <p>        &lt;div class="wrapper"&gt;</p>
+                            {/* TEXT CONTAINER */}
+                            <p>            &lt;div class="textContainer"&gt;</p>
+                            <p>                &lt;h1&gt;{userText.name}&lt;/h1&gt;</p>
+                            <p>                &lt;h2&gt;{userText.subtitle}&lt;/h2&gt;</p>
+                            {/* Map through links */}
+                            {inputFields.map((field, index) => {
+                                return (
+                                    <div key={index}>
+                                    <p>                &lt;a href="{field.link}"&gt;{field.websiteName}&lt;/a&gt;</p>
+                                    </div>
+                                )
+                            })}              
+                            <p>            &lt;/div&gt;</p>
+                            <p>    &lt;/div&gt;</p>     
+                            <p>&lt;/body&gt;</p>
+                            <p>&lt;/html&gt;</p>
+                        </pre>
                     </div>
 
                     <div className="copyCss">
@@ -47,7 +49,7 @@ const Download = ({userBackground, userText, inputFields}) => {
                             
 
                     </div>{/* END copyCss */}
-                </div>{/* END userCOPY */}
+                </div>{/* END userCopy */}
             </div>
         </section>
     )
