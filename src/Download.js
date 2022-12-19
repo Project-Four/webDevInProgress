e4afa12e1ef09716eb1108503d1bc0c49a7fdb44
MandeepMann <mandeep_mann30@hotmail.com>
2022-12-19 12:12:06 -0500

    deleted old copying html section

--- src/Download.js
+++ src/Download.js
@@ -10,56 +10,17 @@ const Download = ({userBackground, userText, inputFields}) => {
 
                 <div className="userCopy">
                     <div className="copyHtml">
-                    <pre><code className="language-markup">
-                    &lt;!DOCTYPE html&gt;<br/>&lt;html lang="en"&gt;<br/>&lt;head&gt;<br/>    &lt;meta charset="UTF-8" /&gt;<br/>    &lt;meta http-equiv="X-UA-Compatible" content="IE=edge" /&gt;<br/>    &lt;meta name="viewport" content="width=device-width, initial-scale=1.0" /&gt;<br/>    &lt;title&gt;Document&lt;/title&gt;<br/>&lt;/head&gt;<br/>&lt;body&gt;<br/>    &lt;div class="backgroundContainer"&gt;<br/>        &lt;div class="wrapper"&gt;<br/>            &lt;div class="textContainer"&gt;<br/>                &lt;h1 class="title"&gt;&lt;/h1&gt;<br/>                &lt;h2 class="subtitle"&gt;&lt;/h2&gt;<br/>
-                    {inputFields.map((field) => { return (
-                        <>                &lt;a href="{field.link}"&gt;{field.websiteName}&lt;/a&gt;<br/></>        
-                        )
-                    })}            &lt;/div&gt;<br/>        &lt;/div&gt;<br/>    &lt;/div&gt;<br/>&lt;/body&gt;<br/>&lt;/html&gt;<br/>
-                    </code></pre>
-                        
-
-
-                        {/* HEAD */}
-                        <pre>
-                            <p>&lt;!DOCTYPE html&gt;</p>
-                            <p>&lt;html lang="en"&gt;</p>
-                            <p>&lt;head&gt;</p>
-                            <p>    &lt;meta charset="UTF-8" /&gt;</p>
-                            <p>    &lt;meta http-equiv="X-UA-Compatible" content="IE=edge" /&gt;</p>
-                            <p>    &lt;meta name="viewport" content="width=device-width, initial-scale=1.0" /&gt;</p>
-                            <p>    &lt;title&gt;Document&lt;/title&gt;</p>
-                            <p>&lt;/head&gt;</p>
-
-                            {/* BODY */}
-                            <p>&lt;body&gt;</p>
-                            {/* BACKGROUND CONTAINER */}
-                            <p>    &lt;div class="backgroundContainer"&gt;</p>
-                            {/* WRAPPER */}
-                            <p>        &lt;div class="wrapper"&gt;</p>
-                            {/* TEXT CONTAINER */}
-                            <p>            &lt;div class="textContainer"&gt;</p>
-                            <p>                &lt;h1&gt;{userText.name}&lt;/h1&gt;</p>
-                            <p>                &lt;h2&gt;{userText.subtitle}&lt;/h2&gt;</p>
-                            {/* Map through links */}
-                            {inputFields.map((field, index) => {
-                                return (
-                                    <div key={index}>
-                                    <p>                &lt;a href="{field.link}"&gt;{field.websiteName}&lt;/a&gt;</p>
-                                    </div>
-                                )
-                            })}              
-                            <p>            &lt;/div&gt;</p>
-                            <p>    &lt;/div&gt;</p>     
-                            <p>&lt;/body&gt;</p>
-                            <p>&lt;/html&gt;</p>
-                        </pre>
+                        <pre><code className="language-markup">
+                        &lt;!DOCTYPE html&gt;<br/>&lt;html lang="en"&gt;<br/>&lt;head&gt;<br/>    &lt;meta charset="UTF-8" /&gt;<br/>    &lt;meta http-equiv="X-UA-Compatible" content="IE=edge" /&gt;<br/>    &lt;meta name="viewport" content="width=device-width, initial-scale=1.0" /&gt;<br/>    &lt;title&gt;Document&lt;/title&gt;<br/>&lt;/head&gt;<br/>&lt;body&gt;<br/>    &lt;div class="backgroundContainer"&gt;<br/>        &lt;div class="wrapper"&gt;<br/>            &lt;div class="textContainer"&gt;<br/>                &lt;h1 class="title"&gt;&lt;/h1&gt;<br/>                &lt;h2 class="subtitle"&gt;&lt;/h2&gt;<br/>
+                        {inputFields.map((field) => { return (
+                            <>                &lt;a href="{field.link}"&gt;{field.websiteName}&lt;/a&gt;<br/></>        
+                            )
+                        })}            &lt;/div&gt;<br/>        &lt;/div&gt;<br/>    &lt;/div&gt;<br/>&lt;/body&gt;<br/>&lt;/html&gt;<br/>
+                        </code></pre>
                     </div>
 
                     <div className="copyCss">
-                    
-                            
-
+                        <p>CSS GOES HERE!</p>
                     </div>{/* END copyCss */}
                 </div>{/* END userCopy */}
             </div>
@@ -68,4 +29,3 @@ const Download = ({userBackground, userText, inputFields}) => {
 }
 
 export default Download
-
