070155ccf8485b12827d0b3b770c3c45a82e374f
MandeepMann <mandeep_mann30@hotmail.com>
2022-12-21 20:03:02 -0500

    added rel norefferer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="wrapper">
-                <p className="footer">Created at <a target="_blank" rel="noreferrer" href="https://junocollege.com">Juno College of Technology</a> by <a target="_blank" href="https://github.com/MandeepKMann">Mandeep Mann</a>, <a target="_blank" href="https://github.com/esteraolszewski">Estera Olszewski</a>, <a target="_blank" href="https://github.com/Justinng26">Justin Ng</a>, and <a target="_blank" href="https://github.com/TTH25N">Thomas Huynh</a></p>
+                <p className="footer">Created at <a target="_blank" rel="noreferrer" href="https://junocollege.com">Juno College of Technology</a> by <a target="_blank" rel="noreferrer" href="https://github.com/MandeepKMann">Mandeep Mann</a>, <a target="_blank" rel="noreferrer" href="https://github.com/esteraolszewski">Estera Olszewski</a>, <a target="_blank"  rel="noreferrer"href="https://github.com/Justinng26">Justin Ng</a>, and <a target="_blank"  rel="noreferrer"href="https://github.com/TTH25N">Thomas Huynh</a></p>
             </div>
         </footer>
     )
